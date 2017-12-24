719a3b5185b6456e85718614dcdf35864fc39e9d
Andrés <drass.apps@gmail.com>
2017-12-24 17:29:03 +0100

    Nav View iOS

---
+++ NavigationViewiOS/NavigationViewiOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NavigationViewiOS
+//
+//  Created by Andrés on 24/12/17.
+//  Copyright © 2017 Andrés. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Actiamos el boton del Nav
+        self.addSlideMenuButton()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NavigationViewiOS/NavigationViewiOS/Views/Vista2ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  Vista2ViewController.swift
+//  NavigationViewiOS
+//
+//  Created by Andrés on 24/12/17.
+//  Copyright © 2017 Andrés. All rights reserved.
+//
+
+import UIKit
+
+class Vista2ViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Actiamos el boton del Nav
+        self.addSlideMenuButton()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
